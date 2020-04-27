0495b6d3aab33ec8fe1386aeb828b7dbd3fb3192
Marina <eainere@yandex.ru>
2020-04-27 19:53:00 +0300

    Add JavaDOC

--- src/main/java/lesson02/task01/CustomException.java
+++ src/main/java/lesson02/task01/CustomException.java
@@ -2,9 +2,16 @@ package lesson02.task01;
 
 /**
  * @author Marina_Larionova
+ * @version 1.0.0
+ * <p>
+ * Класс с исключением.
  */
 public class CustomException extends RuntimeException {
 
+    /**
+     * Конструктор класса
+     * @param message - сообщение, которое необходимо вывести
+     */
     public CustomException(String message) {
         super(message);
     }
--- src/main/java/lesson02/task01/HelloWorld.java
+++ src/main/java/lesson02/task01/HelloWorld.java
@@ -5,33 +5,61 @@ import java.util.List;
 
 /**
  * @author Marina_Larionova
- * @version 1.0
+ * @version 1.0.0
+ * <p>
+ * Класс с точкой входа. В ходе выполнения программы выбрасывает исключения и завершается с ошибкой.
  */
 
 public class HelloWorld {
 
+    /**
+     * Главный метод программы
+     *
+     * @params args  - параметры командной строки
+     */
     public static void main(String[] args) {
+        /**
+         * Переменная list - список строковых значений
+         */
         List<String> list = CreateArrayList();
 
+        /**
+         * Вызов IndexOutOfBoundsException при получения элемента с индексом за пределами массива
+         */
         try {
             list.get(10);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("IndexOutOfBoundsException");
             e.printStackTrace();
         }
+        /**
+         * Вызов NullPointerException при попытке работы со значением null
+         */
         try {
             System.out.println(list.get(5).split(" "));
         } catch (NullPointerException e1) {
             System.out.println("NullPointerException");
             e1.printStackTrace();
+        /**
+         * Вызов CustomException с сообщением "Custom Message"
+         */
         } finally {
             System.out.println("CustomException");
             throw new CustomException("Custom Message");
         }
     }
 
+    /**
+     * Метод для создания списка значений
+     *
+     * @return list - список строк
+     */
     private static List<String> CreateArrayList() {
+        /**
+         * Переменная list - список строковых значений
+         */
         List<String> list = new ArrayList<String>();
+
         for (int i = 0; i < 5; i++) {
             list.add(String.valueOf(i));
         }
