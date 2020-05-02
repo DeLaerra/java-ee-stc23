fa9f382ee9e728e0a8d76e3e4191dca51a10bd0b
Marina <eainere@yandex.ru>
2020-05-02 14:57:03 +0300

    Method names fix int k to long k fix JavaDOC fix

--- src/main/java/lesson02/task02/Main.java
+++ src/main/java/lesson02/task02/Main.java
@@ -4,51 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
+
 /**
- * @author Marina_Larionova
- * @version 1.0.0
- * <p>
  * Генератор N случайных чисел, вычисляющий для каждого числа квадратный корень.
  * Если квадрат целой части числа равен исходному числу, это число выводится на экран.
  * Если первоначальные числа отрицательные, в этом случае генерируется исключение.
+ *
+ * @author Marina_Larionova
+ * @version 1.0.0
  */
 
 public class Main {
-    /**
-     * Приватная переменная nArray - массив целых чисел
-     */
     private int[] nArray;
 
-    /**
-     * Геттер для переменной nArray, возвращает значение переменной nArray
-     * @return nArray
-     */
     public int[] getNArray() {
         return nArray;
     }
 
-    /**
-     * Сеттер для переменной nArray, устанавливает значение переменной nArray
-     * @param nArray
-     */
     public void setNArray(int[] nArray) {
         this.nArray = nArray;
     }
 
-    /**
-     * Главный метод программы
-     *
-     * @params args  - параметры командной строки
-     */
     public static void main(String[] args) {
-        /**
-         * Переменная main для создания экземпляра класса Main
-         */
         Main main = new Main();
-        main.setNArray(main.CreateArray());
+        main.setNArray(main.createArray());
 
         try {
-            main.CalculateSquareRoot(main.getNArray());
+            main.calculateSquareRoot(main.getNArray());
         } catch (ArithmeticException e) {
             e.printStackTrace();
         }
@@ -56,21 +38,12 @@ public class Main {
 
     /**
      * Метод, создающий целочисленный массив случайных элементов с длиной, указанной пользователем
+     *
      * @return nArray - массив случайных чисел
      */
-    private int[] CreateArray() {
-        /**
-         * Переменная reader для чтения строковых данных с консоли
-         */
+    private int[] createArray() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        /**
-         * Переменная n для перевода строкового значения переменной reader в целое число - длину массива
-         */
         int n = 0;
-        /**
-         * Переменная random для заполнения массива случайными значениями
-         */
         Random random = new Random();
 
         try {
@@ -93,8 +66,8 @@ public class Main {
      * @param array - целочисленный массив
      * @throws ArithmeticException - исключение при попытке извлечь корень из отрицательного числа
      */
-    private void CalculateSquareRoot(int[] array) throws ArithmeticException {
-        for (int k : array) {
+    private void calculateSquareRoot(int[] array) throws ArithmeticException {
+        for (long k : array) {
             if (k < 0) throw new ArithmeticException("Невозможно извлечь корень из отрицательного числа!");
             double s = Math.sqrt(k);
             if (k == (((int) s) * ((int) s))) {
